152746dfe05894733b721e89461054e95c98d6dc
AlexMikhaylov <alx.mikhaylov@gmail.com>
2024-08-31 23:48:19 +0200

    Update Kotlin version and switch from .properties to .yml for application file

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
+	kotlin("jvm") version "2.0.20"
+	kotlin("plugin.spring") version "2.0.20"
 	id("org.springframework.boot") version "3.4.0-SNAPSHOT"
 	id("io.spring.dependency-management") version "1.1.6"
 	java
